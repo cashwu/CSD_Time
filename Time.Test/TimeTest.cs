cae64c60ad9e6002bd85baa8d9c49b27f50fb539
cash <cash@cashwu.com>
2016-05-27 13:42:51 +0800

    init

---
+++ Time.Test/TimeTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Time.Test
+{
+    [TestClass]
+    public class TimeTest
+    {
+        [TestMethod]
+        public void GetNowStringTest()
+        {
+            var dt = DateTime.Now;
+            var t = new Time(dt);
+            var result = t.GetNowString();
+
+            var expect = dt.ToString("yyyy/MM/dd HH:mm:ss fff");
+
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
---
+++ Time/Time.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Time
+{
+    public class Time
+    {
+        private DateTime dt;
+
+        public Time()
+        {
+            this.dt = DateTime.Now;
+        }
+
+        public Time(DateTime dt)
+        {
+            this.dt = dt;
+        }
+
+        public string GetNowString()
+        {
+            return dt.ToString("yyyy/MM/dd HH:mm:ss fff");
+        }
+    }
+}
